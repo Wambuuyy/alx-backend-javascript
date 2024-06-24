592c6f8bd574237012f82dd976a9cb4d5f57de92
Wambuuyy <wambuiprudy@gmail.com>
2024-06-25 03:18:59 +0800

    corrections for task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,3 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  let employees = [];
-  for (const department of Object.values(report.allEmployees)) {
-    employees.push(...department);
-  }
-  return employees;
+  return Object.values(report.allEmployees).flat();
 }
