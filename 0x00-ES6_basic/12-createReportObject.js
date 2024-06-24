c4f758f1cb52ce4800e3833e073dfc1511ebc8d7
Wambuuyy <wambuiprudy@gmail.com>
2024-06-25 03:10:43 +0800

    correction task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,4 @@
-import employeesList from './11-createEmployeesObject.js';
-
 export default function createReportObject(employeesList) {
-  const departments = Object.keys(employeesList);
-  return {
-    allEmployees: employeesList,
-    getNumberOfDepartments() {
-      return departments.length;
-    },
-  };
+  const departments = () => Object.keys(employeesList).length;
+  return { allEmployees: employeesList, departments };
 }
