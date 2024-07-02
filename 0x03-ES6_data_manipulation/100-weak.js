d76a41bafc0df210c706ea4fd549680bcdc5dcec
Wambuuyy <wambuiprudy@gmail.com>
2024-07-03 03:16:21 +0800

    remove lint 2

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,13 +4,13 @@ export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
   }
-  
+
   let count = weakMap.get(endpoint);
   count += 1;
-  
+
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  
+
   weakMap.set(endpoint, count);
 }
